e26729697fa84dfffe6a7dafabbcc8d9253960e5
Pablo <Pablo@PJLV>
2024-01-26 08:55:40 +0100

    constructor creado

---
+++ src/vector/MiVector.java
@@ -0,0 +1,31 @@
+package vector;
+
+import java.util.Arrays;
+
+public class MiVector {
+
+	private int vector[];
+	int cuantos;
+
+	public MiVector(int cantidad) {
+		vector = new int[cantidad];
+		cuantos = 0;
+
+	}
+	
+	public int add(int num) {
+		
+		
+		return cuantos;
+	}
+	
+	
+
+	@Override
+	public String toString() {
+		return   Arrays.toString(vector) + " cuantos = " + cuantos ;
+	}
+
+	
+	
+}
---
+++ src/vector/Start.java
@@ -0,0 +1,13 @@
+package vector;
+
+public class Start {
+
+	public static void main(String[] args) {
+
+		
+		MiVector uno = new MiVector(10);
+		
+		System.out.println(uno);
+	}
+
+}
