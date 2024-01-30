3a16d058545699a2ab4cdfc4c1587c04e53c12c8
Pablo <Pablo@PJLV>
2024-01-30 20:53:11 +0100

    ejercicio finalizado

--- src/vector/MiVector.java
+++ src/vector/MiVector.java
@@ -4,28 +4,66 @@ import java.util.Arrays;
 
 public class MiVector {
 
-	private int vector[];
+	private int datos[];
 	int cuantos;
 
 	public MiVector(int cantidad) {
-		vector = new int[cantidad];
+		datos = new int[cantidad];
 		cuantos = 0;
 
 	}
-	
+
 	public int add(int num) {
-		
-		
-		return cuantos;
+
+		if (cuantos < datos.length) {
+
+			datos[cuantos] = num;
+			cuantos++;
+			return cuantos - 1;
+		}
+
+		return -1;
 	}
-	
-	
+
+	public int del(int num) {
+
+		int pos = buscar(num);
+		if (pos != -1) {
+			if (pos < cuantos - 1) {
+				for (int i = pos; i <= cuantos - 1; i++) {
+					datos[i] = datos[i + 1];
+				}
+				cuantos--;
+
+			}
+
+		}
+
+		return pos;
+	}
+
+	private int buscar(int num) {
+		int pos = -1;
+		for (int i = 0; i < datos.length; i++) {
+			if (datos[i] == num) {
+				pos = i;
+			}
+		}
+		return pos;
+	}
 
 	@Override
 	public String toString() {
-		return   Arrays.toString(vector) + " cuantos = " + cuantos ;
+		String devolver = "[";
+
+		for (int i = 0; i < cuantos - 1; i++) {
+
+			devolver += "" + datos[i] + ", ";
+
+		}
+
+		return devolver + datos[cuantos - 1] + "]" + "\ncuantos: " + cuantos;
+
 	}
 
-	
-	
 }
--- src/vector/Start.java
+++ src/vector/Start.java
@@ -7,6 +7,17 @@ public class Start {
 		
 		MiVector uno = new MiVector(10);
 		
+		uno.add(1);
+		uno.add(2);
+		uno.add(3);
+		uno.add(4);
+		uno.add(5);
+		
+		uno.del(1);
+		uno.del(2);
+		uno.del(3);
+			
+		
 		System.out.println(uno);
 	}
 
